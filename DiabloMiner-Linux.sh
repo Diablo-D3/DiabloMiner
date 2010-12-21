3743727d9fb13b8a099b1ea925a172cd41470047
Patrick McFarland <diablod3@gmail.com>
2010-12-21 08:40:44 -0500

    Added environment variable that fixes Stream SDK 2.2/2.3 CPU use bug

--- DiabloMiner-Linux.sh
+++ DiabloMiner-Linux.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export GPU_USE_SYNC_OBJECTS=1
 java -cp target/libs/*:target/DiabloMiner-0.0.1-SNAPSHOT.jar -Djava.library.path=target/libs/natives/linux com.diablominer.DiabloMiner.DiabloMiner $@
