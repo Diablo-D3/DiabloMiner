a7b9fafa8074071bb6dfa748d71b00486e4ed6e3
Patrick McFarland <diablod3@gmail.com>
2012-11-12 19:29:13 -0500

    Updated LWJGL to 2.8.5, switch to official Maven packages

--- DiabloMiner-OSX.sh
+++ DiabloMiner-OSX.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export GPU_USE_SYNC_OBJECTS=1
 cd $(dirname ${0})
-exec java -Xmx32m -cp target/libs/*:target/DiabloMiner.jar -Djava.awt.headless=true -Djava.library.path=target/libs/natives/macosx com.diablominer.DiabloMiner.DiabloMiner $@
+exec java -Xmx32m -cp target/libs/*:target/DiabloMiner.jar -Djava.awt.headless=true -Djava.library.path=target/libs/natives com.diablominer.DiabloMiner.DiabloMiner $@
